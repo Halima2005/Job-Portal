702f559540e353a1bc56cd06884f73f2ef21fc58
Halima2005 <halimayasmin6579@gmail.com>
2025-01-30 11:40:16 -0500

    version 5

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import React, { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import RecruiterLogin from "./components/RecruiterLogin.jsx";
 import { AppContext } from "./context/AppContext.jsx";
+import AddJob from "./pages/AddJob.jsx";
 import Application from "./pages/Application.jsx";
 import ApplyJob from "./pages/ApplyJob.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 import Home from "./pages/Home.jsx";
+import ManageJobs from "./pages/ManageJobs.jsx";
+import ViewApplications from "./pages/ViewApplications.jsx";
 
 const App = () => {
   const { showRecruiterLogin } = useContext(AppContext);
@@ -16,6 +20,11 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/apply-job/:id" element={<ApplyJob />} />
         <Route path="/application" element={<Application />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="manage-jobs" element={<ManageJobs />} />
+          <Route path="view-applications" element={<ViewApplications />} />
+        </Route>
       </Routes>
     </div>
   );
